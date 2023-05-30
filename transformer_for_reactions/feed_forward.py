d3577ee63d8bb30069130768e44064fa9f1b4d60
brendaferrari <brendafferrari@gmail.com>
2023-05-30 16:43:31 -0300

    refactor: add ffw

---
+++ transformer_for_reactions/feed_forward.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class FeedFoward(nn.Module):
+
+    def __init__(self, emb_dim, dropout):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(emb_dim, 4 * emb_dim),
+            nn.ReLU(),
+            nn.Linear(4 * emb_dim, emb_dim),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
